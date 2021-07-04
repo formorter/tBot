a22d640e83e967a07ef5fe7b90439f1f529e8b83
auisze <pponyatskii322@gmail.com>
2021-07-05 00:34:09 +0300

    сделал меню, но бот не запускается
    я поменял немного код, добавил менюшку с функцией, но бот перестал запускаться. я хочу добавить ссылку, но я пока не знаю как

--- tbot/bot.py
+++ tbot/bot.py
@@ -1,17 +1,38 @@
-import telebot
+import telebot 
+from telebot import types
 
 TOKEN = '1782289333:AAF66r8nfRumURI9O1Dvv_mLc5rBh_4OZh0'
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+
+    item1 = types.KeyboardButton('Спотифай')
+
+    markup.add(item1)
+
+    bot.send_message(message.chat.id, 'Привет, {0.first_name}!'.format(message.from_user), reply_markup = markup)
 
 @bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.from_user.id, 'Привет!')
-    else:
-        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+def bot_message(message):
+    if message.text == 'Спотифай':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+        item1 = types.KeyboardButton('Наш плейлист')
+        back = types.KeyboardButton('Назад')
+        markup.add(item1, back)
+        bot.send_message(message.chat.id, 'https://open.spotify.com/playlist/5BQemH4tSKWnOeUjOGGCJW')
+
+
+    elif message.text == 'Назад':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+
+        item1 = types.KeyboardButton('Спотифай')
+
+        markup.add(item1)
+
+        bot.send_message(message.chat.id, 'Назад', reply_markup = markup)
+
+        
 
 bot.polling(none_stop=True)
