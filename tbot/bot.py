6c93c8eabc5f766ca9327144f4311d3633aadf47
Ilareon <86919690+Ilareon@users.noreply.github.com>
2021-07-04 21:33:15 +0300

    Команда /help
    теперь бот при команде /help отвечает по-другому.

--- tbot/bot.py
+++ tbot/bot.py
@@ -3,9 +3,12 @@ import telebot
 TOKEN = '1782289333:AAF66r8nfRumURI9O1Dvv_mLc5rBh_4OZh0'
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+    bot.send_message(message.from_user.id, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.send_message(message.from_user.id, f'Помощи пока нет.')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
