936f1abf2a4d81cc3fec90f233214e118d3793c2
auisze <pponyatskii322@gmail.com>
2021-07-05 10:28:01 +0300

    всё, я исправил
    есть меню с одной кнопкой, которая выдаёт ссылку на спотик

--- tbot/bot.py
+++ tbot/bot.py
@@ -15,23 +15,14 @@ def start(message):
     bot.send_message(message.chat.id, 'Привет, {0.first_name}!'.format(message.from_user), reply_markup = markup)
 
 @bot.message_handler(content_types=['text'])
+
 def bot_message(message):
+
+
     if message.text == 'Спотифай':
         markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-        item1 = types.KeyboardButton('Наш плейлист')
-        back = types.KeyboardButton('Назад')
-        markup.add(item1, back)
-        bot.send_message(message.chat.id, 'https://open.spotify.com/playlist/5BQemH4tSKWnOeUjOGGCJW')
-
-
-    elif message.text == 'Назад':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-
-        item1 = types.KeyboardButton('Спотифай')
-
         markup.add(item1)
-
-        bot.send_message(message.chat.id, 'Назад', reply_markup = markup)
+        bot.send_message(message.chat.id, 'https://open.spotify.com/playlist/5BQemH4tSKWnOeUjOGGCJW')
 
         
 
