9579bd9f5af0a175fdb19dbc6e178bd7465a5725
formorter <72940774+formorter@users.noreply.github.com>
2021-07-06 14:44:48 +0300

    add /help, /music

--- telegramm/Spoti.py
+++ telegramm/Spoti.py
@@ -5,14 +5,12 @@ TOKEN = '1742555924:AAGRqx3iu9TKqVSA82CGuzpnYQ2oA2Ln4Ws'
 bot = telebot.TeleBot(TOKEN)
 
 
-class Spotify:
-    def __init__(self, message):
-        self.message = message
-        self.markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+@bot.message_handler(commands=['Музыка'])
+def spotify(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 
-    @bot.message_handler(content_types=['text'])
-    def mess(self):
-        item1 = types.KeyboardButton(f'{self.message}')
-        self.markup.add(item1)
+    item1 = types.KeyboardButton('Спотифай')
 
+    markup.add(item1)
 
+    bot.send_message(message.chat.id, 'На какой платформе будем слушать?'.format(message.from_user), reply_markup=markup)
