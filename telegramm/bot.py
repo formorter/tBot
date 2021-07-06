e8a4a0ea13a773afe74e1a78dcca367f9f521b31
formorter <72940774+formorter@users.noreply.github.com>
2021-07-06 14:45:41 +0300

    Revert "Revert "add /help, /music""
    This reverts commit 39c6f1cf2425ad0d0c5f3aa92ce9a5e89c9d6987.

--- telegramm/bot.py
+++ telegramm/bot.py
@@ -1,24 +1,35 @@
 import telebot
 from telebot import types
 from telegramm.Logger import BotLogger
-from telegramm.Spoti import Spotify
+from telegramm.Spoti import spotify
 
-TOKEN = '1782289333:AAF66r8nfRumURI9O1Dvv_mLc5rBh_4OZh0'
+TOKEN = '1742555924:AAGRqx3iu9TKqVSA82CGuzpnYQ2oA2Ln4Ws'
 bot = telebot.TeleBot(TOKEN)
 
 start_logger = BotLogger()
 start_logger.main('Бот запущен, приятного пользования')
 
 
-@bot.message_handler(commands=['start', 'help'])
-def start(message):
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, f'Я AXIUS, для того, чтобы узнать, что я умею, используй /help')
+
+
+@bot.message_handler(commands=['music'])
+def spotify(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 
     item1 = types.KeyboardButton('Спотифай')
 
     markup.add(item1)
 
-    bot.send_message(message.chat.id, 'Привет, {0.first_name}!'.format(message.from_user), reply_markup=markup)
+    bot.send_message(message.chat.id, 'На какой платформе будем слушать?'.format(message.from_user),
+                     reply_markup=markup)
+
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, f'На данный момент имеется только команда /music')
 
 
 @bot.message_handler(content_types=['text'])
@@ -31,5 +42,6 @@ def bot_message(message):
     elif message.text == 'Плейлист':
         bot.send_message(message.chat.id, 'https://open.spotify.com/playlist/5BQemH4tSKWnOeUjOGGCJW',
                          reply_markup=markup)
+
 
 bot.polling(none_stop=True)
